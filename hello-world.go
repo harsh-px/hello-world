c6ce9e13d867333d9dba013d1db06c75ebdf1e44
Harsh Desai <harsh@portworx.com>
2018-07-12 16:22:17 -0700

    most of the build is passing
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- hello-world.go
+++ hello-world.go
@@ -6,21 +6,123 @@ import (
 	"os"
 	"time"
 
-	"github.com/portworx/sched-ops/k8s"
+	"github.com/kubernetes/kubernetes/pkg/kubectl/cmd"
 	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 )
+
+func initK8sClient() (*kubernetes.Clientset, error) {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if len(kubeconfig) == 0 {
+		return nil, fmt.Errorf("kubeconfig is not set")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	// Quick validation if client connection works
+	_, err = client.ServerVersion()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to k8s server: %s", err)
+	}
+
+	return client, nil
+}
+
+func PrintPodLogs(podName, podNamespace string) error {
+	client, err := initK8sClient()
+	if err != nil {
+		return err
+	}
+}
+
+func RunCommandInPod(cmds []string, podName, containerName, namespace string, stdout, stderr io.Writer, in io.Reader) (string, error) {
+	client, err := initK8sClient()
+	if err != nil {
+		return "", err
+	}
+
+	pod, err := client.Core().Pods(namespace).Get(podName, meta_v1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	if len(containerName) == 0 {
+		if len(pod.Spec.Containers) != 1 {
+			return "", fmt.Errorf("could not determine which container to use")
+		}
+
+		containerName = pod.Spec.Containers[0].Name
+	}
+
+	req := client.Core().RESTClient().Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).
+		SubResource("exec").
+		Param("container", containerName)
+
+	req.VersionedParams(&v1.PodExecOptions{
+		Container: containerName,
+		Command:   cmds,
+		Stdin:     true,
+		Stdout:    true,
+		Stderr:    true,
+		TTY:       true,
+	}, scheme.ParameterCodec)
+
+	execOpts := &cmd.ExecOptions{
+		StreamOptions: cmd.StreamOptions{
+			In:  in,
+			Out: stdout,
+			Err: stderr,
+		},
+		Executor: &cmd.DefaultRemoteExecutor{},
+	}
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		return "", fmt.Errorf("failed to init executor: %v", err)
+	}
+
+	fn := func() error {
+		err = exec.Stream(remotecommand.StreamOptions{
+			Stdin:  in,
+			Stdout: stdout,
+			Stderr: stderr,
+			Tty:    true,
+		})
+		if err != nil {
+			return fmt.Errorf("could not execute: %v", err)
+		}
+
+		return nil
+	}
+
+	return "", fn()
+}
 
 //func demoRunPodCmds(podname, podnamespace string, cmds []string, in *bufio.Reader) (string, error) {
 func demoRunPodCmds(podname, podnamespace string, cmds []string, stdout, stderr io.Writer, stdin io.Reader) (string, error) {
 	fmt.Printf("[debug] foo...\n")
-	inst := k8s.Instance()
 	/*input, err := in.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
 
 	fmt.Printf("input: %s\n", input)*/
-	output, err := inst.RunCommandInPod(cmds, podname, "", podnamespace, stdout, stderr, stdin)
+	output, err := RunCommandInPod(cmds, podname, "", podnamespace, stdout, stderr, stdin)
 	if err != nil {
 		return "", err
 	}
